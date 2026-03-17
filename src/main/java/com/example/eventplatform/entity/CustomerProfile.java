73b6021884c0488b56c4938726ba94af3d90c1a1
brooky <tomishing@gmail.com>
2026-03-17 00:06:52 -0700

    Added booking.css, confirmation.html, planner.html, thankyou.html. Modified SecurityConfig.java in order to access /booking/**.

--- src/main/java/com/example/eventplatform/entity/CustomerProfile.java
+++ src/main/java/com/example/eventplatform/entity/CustomerProfile.java
@@ -129,5 +129,6 @@ public class CustomerProfile {
     }
 
     public void setName(String temporaryCustomer) {
+
     }
 }
--- src/main/java/com/example/eventplatform/entity/OrganizerProfile.java
+++ src/main/java/com/example/eventplatform/entity/OrganizerProfile.java
@@ -139,5 +139,6 @@ public class OrganizerProfile {
     }
 
     public void setName(String temporaryOrganizer) {
+
     }
 }
