d1434deb1ab1c864cc7f252d9f4337153c07b395
H Masuyama <58389623+masuyama13@users.noreply.github.com>
2026-04-01 01:27:10 -0700

    Customer/Organizer Registration  (#13)
    * Implement customer and organizer registration functionality
    
    * Delete organizers/new

--- src/main/java/com/example/eventplatform/controller/OrganizerController.java
+++ src/main/java/com/example/eventplatform/controller/OrganizerController.java
@@ -1,15 +1,10 @@
 package com.example.eventplatform.controller;
 
 import com.example.eventplatform.entity.OrganizerProfile;
-import com.example.eventplatform.entity.User;
-import com.example.eventplatform.entity.UserRole;
-import com.example.eventplatform.repository.UserRepository;
 import com.example.eventplatform.service.OrganizerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.List;
 
 @Controller
@@ -17,12 +12,9 @@ import java.util.List;
 public class OrganizerController {
 
     private final OrganizerService organizerService;
-    private final UserRepository userRepository;
 
-    public OrganizerController(OrganizerService organizerService,
-                               UserRepository userRepository) {
+    public OrganizerController(OrganizerService organizerService) {
         this.organizerService = organizerService;
-        this.userRepository = userRepository;
     }
 
     // LIST
@@ -39,20 +31,6 @@ public class OrganizerController {
         OrganizerProfile organizer = organizerService.getOrganizerById(id);
         model.addAttribute("organizer", organizer);
         return "organizer-detail";
-    }
-
-    // CREATE PAGE
-    @GetMapping("/new")
-    public String showCreateForm(Model model) {
-        List<User> organizerUsers = userRepository.findAll()
-                .stream()
-                .filter(user -> user.getRole() == UserRole.ORGANIZER)
-                .toList();
-
-        model.addAttribute("users", organizerUsers);
-        model.addAttribute("organizer", new OrganizerProfile());
-        model.addAttribute("isEdit", false);
-        return "organizer-form";
     }
 
     // CREATE SUBMIT
@@ -79,12 +57,6 @@ public class OrganizerController {
             );
             return "redirect:/organizers";
         } catch (RuntimeException e) {
-            List<User> organizerUsers = userRepository.findAll()
-                    .stream()
-                    .filter(user -> user.getRole() == UserRole.ORGANIZER)
-                    .toList();
-
-            model.addAttribute("users", organizerUsers);
             model.addAttribute("organizer", new OrganizerProfile());
             model.addAttribute("isEdit", false);
             model.addAttribute("error", e.getMessage());
